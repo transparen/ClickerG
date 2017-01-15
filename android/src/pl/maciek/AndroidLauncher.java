bb998f9df44397449b09d34bb6c8b3a1718d43a7
DESKTOP-S2KG25L\mv <majcieks2@gmail.com>
2017-01-16 00:17:12 +0100

    no message

---
+++ android/src/pl/maciek/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.maciek;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.maciek.ClickerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ClickerGame(), config);
+	}
+}
---
+++ desktop/src/pl/maciek/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.maciek.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.maciek.ClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ClickerGame(), config);
+	}
+}
