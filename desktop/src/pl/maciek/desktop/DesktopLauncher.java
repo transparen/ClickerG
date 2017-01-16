8cb4838394061edbdaa3033370c864abfe6d5c8b
DESKTOP-S2KG25L\mv <majcieks2@gmail.com>
2017-01-16 16:22:42 +0100

    config desktop launcher

--- desktop/src/pl/maciek/desktop/DesktopLauncher.java
+++ desktop/src/pl/maciek/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import pl.maciek.ClickerGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = ClickerGame.GAME_NAME;
+		config.width = ClickerGame.WIDTH;
+		config.height = ClickerGame.HEIGHT;
+		config.resizable = false;
+		
+		
 		new LwjglApplication(new ClickerGame(), config);
 	}
 }
