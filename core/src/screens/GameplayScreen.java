cfe17e4f0b3e112fe32f88fc15c50d9b7fb68148
DESKTOP-S2KG25L\mv <majcieks2@gmail.com>
2017-01-16 19:34:09 +0100

    display player image

--- core/src/screens/GameplayScreen.java
+++ core/src/screens/GameplayScreen.java
@@ -1,16 +1,44 @@
 package screens;
 
+import entities.Player;
 import pl.maciek.ClickerGame;
 
 public class GameplayScreen extends AbstractScreen{
-
+	
+	private Player player;
 	public GameplayScreen(ClickerGame game) {
 		super(game);
 		init();
 	}
 
 	private void init() {
+		initPlayer();
 		
+	}
+
+	private void initPlayer() {
+		// TODO Auto-generated method stub
+		player = new Player();
+		stage.addActor(player);
+		
+		
+	}
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		update();
+		
+		spriteBatch.begin();
+		stage.draw();
+		
+		spriteBatch.end();
+		
+		
+	}
+
+	private void update() {
+		// TODO Auto-generated method stub
+		stage.act();
 	}
 
 	
