a39e887cec78ae1875eafcca611ac843911049c1
DESKTOP-S2KG25L\mv <majcieks2@gmail.com>
2017-01-16 19:13:55 +0100

    added dummy gamplay screen

---
+++ core/src/screens/GameplayScreen.java
@@ -0,0 +1,17 @@
+package screens;
+
+import pl.maciek.ClickerGame;
+
+public class GameplayScreen extends AbstractScreen{
+
+	public GameplayScreen(ClickerGame game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+		
+	}
+
+	
+}
