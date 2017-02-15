a97609e6c4422b4d6ab785eca6c22664b1df7f37
DESKTOP-S2KG25L\mv <majcieks2@gmail.com>
2017-02-15 16:26:37 +0100

    added playerButton and clickListener

--- core/src/screens/GameplayScreen.java
+++ core/src/screens/GameplayScreen.java
@@ -1,40 +1,68 @@
 package screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 import entities.Player;
 import pl.maciek.ClickerGame;
 
-public class GameplayScreen extends AbstractScreen{
-	
+public class GameplayScreen extends AbstractScreen {
+
 	private Player player;
+	private Button playerButton;
+
 	public GameplayScreen(ClickerGame game) {
 		super(game);
 	}
+
 	protected void init() {
 		initPlayer();
+		initPlayerButton();
 		
+
 	}
 
-
-	
+	private void initPlayerButton() {
+		// TODO Auto-generated method stub
+		playerButton = new Button(new ButtonStyle());
+		playerButton.setWidth(460);
+		playerButton.setHeight(360);
+		playerButton.setX(10);
+		playerButton.setY(170);
+		playerButton.setDebug(true);
+		
+		stage.addActor(playerButton);
+		playerButton.addListener(new ClickListener(){
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y,
+					int pointer, int button) {
+				System.out.println("click");
+				
+				return super.touchDown(event, x, y, pointer, button);
+			}
+			
+		});
+	}
 
 	private void initPlayer() {
 		// TODO Auto-generated method stub
 		player = new Player();
 		stage.addActor(player);
-		
-		
+
 	}
+
 	@Override
 	public void render(float delta) {
 		super.render(delta);
 		update();
-		
+
 		spriteBatch.begin();
 		stage.draw();
-		
+
 		spriteBatch.end();
-		
-		
+
 	}
 
 	private void update() {
@@ -42,9 +70,4 @@ public class GameplayScreen extends AbstractScreen{
 		stage.act();
 	}
 
-
-
-	
-	
-	
 }
--- core/src/screens/SplashScreen.java
+++ core/src/screens/SplashScreen.java
@@ -6,55 +6,42 @@ import com.badlogic.gdx.utils.Timer.Task;
 
 import pl.maciek.ClickerGame;
 
-public class SplashScreen extends AbstractScreen{
-	
+public class SplashScreen extends AbstractScreen {
+
 	private Texture splashImg;
-	
 
 	public SplashScreen(final ClickerGame game) {
 		super(game);
 		init();
-		
-		Timer.schedule(new Task (){
-			
-					public void run(){
-						System.out.println("Po 1 sekundzie");
-						game.setScreen(new GameplayScreen(game));
-						
-					}
+
+		Timer.schedule(new Task() {
+
+			public void run() {
+				System.out.println("Po 1 sekundzie");
+				game.setScreen(new GameplayScreen(game));
+
+			}
 		}, 1);
-		
+
 	}
 
 	protected void init() {
 		// TODO Auto-generated method stub
-		//TODO   implements better assets loading when game growss
-				splashImg = new Texture("badlogic.jpg");
+		// TODO implements better assets loading when game growss
+		splashImg = new Texture("badlogic.jpg");
 	}
-	
 
-
-	
 	@Override
 	public void render(float delta) {
 		// TODO Auto-generated method stub
 		super.render(delta);
-		
+
 		spriteBatch.begin();
-		
-		
+
 		spriteBatch.draw(splashImg, 0, 0);
-		
+
 		spriteBatch.end();
-		
+
 	}
-
-
-
-
-	
-	
-
-	
 
 }
